8d3c7827baf655f201ddc27e7a3350b4fd039916
Your Name <brooklyn.od.ua@gmai.com>
2019-02-23 16:19:21 +0200

    merge resolve

--- src/main/java/com/yellow/repository/UserRepository.java
+++ src/main/java/com/yellow/repository/UserRepository.java
@@ -1,14 +1,14 @@
 package com.yellow.repository;
 
 import com.yellow.model.User;
-
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
   Optional<User> getAllById(Long userId);
+
+  Optional<User> getAllByLogin(String login);
 }
