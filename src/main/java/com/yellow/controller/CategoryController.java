7a3299c86f98fe694316b08d77a4a6944e8e0dfc
GreySlon <garanenkosergii@gmail.com>
2019-02-23 14:06:54 +0200

    add category service , controller

---
+++ src/main/java/com/yellow/controller/CategoryController.java
@@ -0,0 +1,24 @@
+package com.yellow.controller;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+import com.yellow.model.Category;
+import com.yellow.service.CategoryService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CategoryController {
+
+  @Autowired
+  private CategoryService categoryService;
+
+  @RequestMapping(name = "/category/all", method = GET)
+  public List<Category> getCategories() {
+    return categoryService.getCategories();
+  }
+}
--- src/main/java/com/yellow/model/Category.java
+++ src/main/java/com/yellow/model/Category.java
@@ -2,20 +2,26 @@ package com.yellow.model;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
+import lombok.Data;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import lombok.Data;
 
 @Entity
 @Table(name = "category")
 @Data
 public class Category {
+
   @Id
   @GeneratedValue(strategy = IDENTITY)
+  @JsonProperty(value = "category_id")
   private Long id;
+  @JsonProperty(value = "category_name")
   @Column(name = "name", nullable = false)
   private String name;
 }
