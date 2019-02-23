e3cd8cd804819ef4fcc0907d9b6aa1ddc6e8bc3b
GreySlon <garanenkosergii@gmail.com>
2019-02-23 18:18:02 +0200

    add comment controller

--- src/main/java/com/yellow/controller/CommentController.java
+++ src/main/java/com/yellow/controller/CommentController.java
@@ -1,15 +1,18 @@
 package com.yellow.controller;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
 import com.yellow.domain.AppResponse;
-import com.yellow.domain.CommentDto;
+import com.yellow.domain.CommentDtoIn;
 import com.yellow.service.CommentService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -18,11 +21,16 @@ public class CommentController {
   @Autowired
   private CommentService commentService;
 
-  @RequestMapping(value = "/comment/get/{post_id}/{page}")
+  @RequestMapping(value = "/comment/get/{post_id}/{page}", method = GET)
   public AppResponse getComments(
       @PathVariable("post_id") Long postId,
       @PathVariable("page") Optional<Integer> pageOpt) {
     Integer page = pageOpt.isPresent() ? pageOpt.get() : 0;
     return commentService.getComments(postId, page);
+  }
+
+  @RequestMapping(value = "/comment/add", method = POST)
+  public void addComment(@RequestBody CommentDtoIn commentDtoIn) {
+    commentService.addComment(commentDtoIn);
   }
 }
